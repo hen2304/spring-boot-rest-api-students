8bad7bda6aae1ddf4c1731a8763198a7ea426b99
chen_kas <chen@bambidynamic.com>
2020-10-21 03:18:00 +0300

    new

--- spring-boot-rest-api-tutorial/src/main/java/com/Application.java
+++ spring-boot-rest-api-tutorial/src/main/java/com/Application.java
@@ -1,20 +1,13 @@
-package com.packagestudentscourses;
+package com;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The type Application.
- *
- * @author Chen Kashi
- */
+
 @SpringBootApplication
 public class Application {
 
-  /**
-   * The entry point of application.
-   *
-   * @param args the input arguments   */
-  public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
